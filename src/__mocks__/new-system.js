93a2261a8f88d706f8d94342e347178269bda9fb
mgorem <oremgitonga22@gmail.com>
2022-08-03 13:34:53 +0300

    add new-system json

---
+++ src/__mocks__/new-system.js
@@ -0,0 +1,112 @@
+{
+    "clientsname": "Celestine Atieno Otieno",
+    "buyingprice": 21000,
+    "total": 31907.4,
+    "clientsidnumber": "13686740",
+    "id": 743148,
+    "doe": "3/22/2022",
+    "clientsphone": "0758414284",
+    "sellingprice": 22470,
+    "dailies": 83.888,
+    "deposit": 6741,
+    "addedby": "ivywambui@gmail.com",
+    "period": 300,
+    "phonetype": "Infinix X688B",
+    "payoption": "1",
+    "groupid": "953"
+}, - Device Json
+
+
+{
+    "groupid": "953",
+    "name": "Simon Njuguna",
+    "region": "nairobi",
+    "email": "njugunas246@gmail.com",
+    "intrest": 1.6,
+    "margin": 1.07,
+    "accesslevel": "superadmin"||"admin"||"resident"||"other-agent"
+}, - Agent Json
+
+{
+    "sellingprice": 39590,
+    "total": 56397,
+    "buyingprice": 37000,
+    "dailies": 211.14666666666668,
+    "id": "MLH104",
+    "deposit": 11877,
+    "payoption": "3",
+    "addedby": "njugunas246@gmail.com",
+    "client": "WoFIvUyRByhyG1eX3241",
+    "doe": "22/11/2021",
+    "category": "2",
+    "devicename": "Mika Fridge",
+    "devimei": "28730857",
+    "devphone": "28730857",
+    "period": 210,
+    "groupid": "953"
+}, - Asset Json
+
+{
+    "dor": "4/29/2022",
+    "groupid": "1827",
+    "clientsname": "Victoria Mumbua",
+    "addedby": "podapeklimited@gmail.com",
+    "clientsphone": "0719736214",
+    "clientsidnumber": "2459607-B"
+}, asset client json
+
+
+{     "extra":["nokkra": "A003844945M",
+    "gmobilenumber": "254723167210",
+    "nokname": "Collins Ayacko",
+    "nokphone": "26625109",
+    "gname": "Collins Ayacko"],
+
+    "clientidnumber": "10218566",
+    "clientname": "Rufina Awino",		
+    "security": {"name":"mal052"},
+    "dateofreg": "15/12/2021",
+    "clientmobilenumber": "254706482765"
+}, - customer json
+
+
+{
+    "description": "Friends of Galens",
+    "region": "ahero",
+    "addeon": {
+        "_seconds": 1645776163,
+        "_nanoseconds": 846000000
+    },
+    "chamaid": "mal074",
+    "addedby": "vonyango@gmail.com",
+    "name": "Friends of Galens"
+}, - chama json
+
+{
+    "amountdisbursed": "19600",
+    "intrest": "6000.00",
+    "processingfee": "400",
+    "Period": "90",
+    "dailies": "288.89",
+    "dateofissue": "18/11/2021",
+    "dateofreturn": "16/02/2022",
+    "loancode": "27626843",
+    "amount": "20000",
+    "groupid": "mal033",
+    "payable": "26000.00"
+}, - loan json 
+
+{
+    "date": "04-08-2021 23:58:18",
+    "amount": "10",
+    "transId": "PH467WZNI6",
+    "names": "Simon Njuguna Ndungu",
+    "verified": true,
+    "type": "",
+    "phonenumber": "254745616166",
+    "code": "153369"
+}, transaction json
+
+
+
+
