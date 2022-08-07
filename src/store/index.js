2afd76382521a67c3b3239a5acb53fbfbc5aa4cd
mgorem <oremgitonga22@gmail.com>
2022-08-07 11:40:22 +0300

    remove window error

--- src/store/index.js
+++ src/store/index.js
@@ -12,19 +12,20 @@ const rrfConfig = {
   userProfile: "users",
   useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
 };
+if (typeof window !== "undefined") {
+  const composeEnhancers =
+    process.env.NODE_ENV === "development"
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+      : compose;
 
-const composeEnhancers =
-  process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-    : compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(
-    reactReduxFirebase(firebase, rrfConfig),
-    reduxFirestore(firebase),
-    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
-  )
-);
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(
+      reactReduxFirebase(firebase, rrfConfig),
+      reduxFirestore(firebase),
+      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
+    )
+  );
+}
 
 export default store;
